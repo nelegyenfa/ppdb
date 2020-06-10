4c2b99d076f54e60366f7f862362aef4437996f9
barni9606 <schenkbarnabas@gmail.com>
2020-06-10 18:46:49 +0200

    Initialize project

---
+++ src/main/java/io/nelegyenfa/ppdb/PpdbApplication.java
@@ -0,0 +1,13 @@
+package io.nelegyenfa.ppdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nelegyenfa/ppdb/PpdbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nelegyenfa.ppdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
